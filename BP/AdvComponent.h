3ee622bc08fdafcd1f3e9b01ad3ef6b37fa6d0c2
MythMyth <windy10091996@gmail.com>
2022-05-25 22:28:06 +0700

    Finish adv component class

--- BP/AdvComponent.h
+++ BP/AdvComponent.h
@@ -1,5 +1,28 @@
+#ifndef __ADV_COMP__
+#define __ADV_COMP__
+
 #include<BP.h>
 
 class AdvComponent : public BP {
-    
-}
+    public:
+    AdvComponent(string fname) {
+        fstream fs(fname);
+        string line;
+        getline(fs, line);
+        output = atoi(line.c_str());
+        while(getline(fs, line)) {
+            int pos = line.find(" x ");
+            if(pos == -1) continue;
+            string mat_name = line.substr(0, pos);
+            string mat_num = line.substr(pos+3);
+            material[mat_name] = atoi(mat_num.c_str());
+        }
+        int len = fname.size();
+        name = fname;
+        for(int i = 0; i < len; i++) {
+            if(name[i] == '_') name[i] = ' ';
+        }
+    }
+};
+
+#endif
